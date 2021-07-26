2bcc8132a7b7d2862207f98b3d44ca083c02874b
Mert Gunduz <merttgg@gmail.com>
2021-07-26 11:36:52 +0300

    User Interface First Update

---
+++ MovieX_MainMenu.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace MovieX
+{
+    partial class MovieX_MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mid_Panel = new System.Windows.Forms.Panel();
+            this.Mid_ExpandPanel = new System.Windows.Forms.Panel();
+            this.Top_Panel = new System.Windows.Forms.Panel();
+            this.Top_ExpandPanel = new System.Windows.Forms.Panel();
+            this.Left_Panel = new System.Windows.Forms.Panel();
+            this.Left_ExpandPanel = new System.Windows.Forms.Panel();
+            this.TopLogo_Panel = new System.Windows.Forms.Panel();
+            this.MidUserControl_Panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TopLogo_PictureBox = new System.Windows.Forms.PictureBox();
+            this.Mid_Panel.SuspendLayout();
+            this.Mid_ExpandPanel.SuspendLayout();
+            this.Top_Panel.SuspendLayout();
+            this.Top_ExpandPanel.SuspendLayout();
+            this.Left_Panel.SuspendLayout();
+            this.TopLogo_Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TopLogo_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Mid_Panel
+            // 
+            this.Mid_Panel.Controls.Add(this.Mid_ExpandPanel);
+            this.Mid_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Mid_Panel.Location = new System.Drawing.Point(0, 0);
+            this.Mid_Panel.Name = "Mid_Panel";
+            this.Mid_Panel.Size = new System.Drawing.Size(1100, 700);
+            this.Mid_Panel.TabIndex = 0;
+            // 
+            // Mid_ExpandPanel
+            // 
+            this.Mid_ExpandPanel.Controls.Add(this.MidUserControl_Panel);
+            this.Mid_ExpandPanel.Controls.Add(this.Left_Panel);
+            this.Mid_ExpandPanel.Controls.Add(this.Top_Panel);
+            this.Mid_ExpandPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Mid_ExpandPanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(186)))), ((int)(((byte)(91)))));
+            this.Mid_ExpandPanel.Location = new System.Drawing.Point(0, 0);
+            this.Mid_ExpandPanel.Name = "Mid_ExpandPanel";
+            this.Mid_ExpandPanel.Size = new System.Drawing.Size(1100, 700);
+            this.Mid_ExpandPanel.TabIndex = 0;
+            // 
+            // Top_Panel
+            // 
+            this.Top_Panel.Controls.Add(this.Top_ExpandPanel);
+            this.Top_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Top_Panel.Location = new System.Drawing.Point(0, 0);
+            this.Top_Panel.Name = "Top_Panel";
+            this.Top_Panel.Size = new System.Drawing.Size(1100, 63);
+            this.Top_Panel.TabIndex = 1;
+            // 
+            // Top_ExpandPanel
+            // 
+            this.Top_ExpandPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.Top_ExpandPanel.Controls.Add(this.TopLogo_Panel);
+            this.Top_ExpandPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Top_ExpandPanel.Location = new System.Drawing.Point(0, 0);
+            this.Top_ExpandPanel.Name = "Top_ExpandPanel";
+            this.Top_ExpandPanel.Size = new System.Drawing.Size(1100, 63);
+            this.Top_ExpandPanel.TabIndex = 0;
+            // 
+            // Left_Panel
+            // 
+            this.Left_Panel.Controls.Add(this.Left_ExpandPanel);
+            this.Left_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Left_Panel.Location = new System.Drawing.Point(0, 63);
+            this.Left_Panel.Name = "Left_Panel";
+            this.Left_Panel.Size = new System.Drawing.Size(200, 637);
+            this.Left_Panel.TabIndex = 2;
+            // 
+            // Left_ExpandPanel
+            // 
+            this.Left_ExpandPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.Left_ExpandPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Left_ExpandPanel.Location = new System.Drawing.Point(0, 0);
+            this.Left_ExpandPanel.Name = "Left_ExpandPanel";
+            this.Left_ExpandPanel.Size = new System.Drawing.Size(200, 637);
+            this.Left_ExpandPanel.TabIndex = 0;
+            // 
+            // TopLogo_Panel
+            // 
+            this.TopLogo_Panel.Controls.Add(this.label1);
+            this.TopLogo_Panel.Controls.Add(this.TopLogo_PictureBox);
+            this.TopLogo_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TopLogo_Panel.Location = new System.Drawing.Point(0, 0);
+            this.TopLogo_Panel.Name = "TopLogo_Panel";
+            this.TopLogo_Panel.Size = new System.Drawing.Size(200, 63);
+            this.TopLogo_Panel.TabIndex = 0;
+            // 
+            // MidUserControl_Panel
+            // 
+            this.MidUserControl_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.MidUserControl_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MidUserControl_Panel.Location = new System.Drawing.Point(200, 63);
+            this.MidUserControl_Panel.Name = "MidUserControl_Panel";
+            this.MidUserControl_Panel.Size = new System.Drawing.Size(900, 637);
+            this.MidUserControl_Panel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(40, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.label1.Size = new System.Drawing.Size(160, 63);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MovieX";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TopLogo_PictureBox
+            // 
+            this.TopLogo_PictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TopLogo_PictureBox.Image = global::MovieX.Properties.Resources.MovieX_TicketIcon;
+            this.TopLogo_PictureBox.Location = new System.Drawing.Point(0, 0);
+            this.TopLogo_PictureBox.Name = "TopLogo_PictureBox";
+            this.TopLogo_PictureBox.Size = new System.Drawing.Size(40, 63);
+            this.TopLogo_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.TopLogo_PictureBox.TabIndex = 2;
+            this.TopLogo_PictureBox.TabStop = false;
+            // 
+            // MovieX_MainMenu
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(229)))), ((int)(((byte)(226)))));
+            this.ClientSize = new System.Drawing.Size(1100, 700);
+            this.Controls.Add(this.Mid_Panel);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MovieX_MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MovieX";
+            this.Mid_Panel.ResumeLayout(false);
+            this.Mid_ExpandPanel.ResumeLayout(false);
+            this.Top_Panel.ResumeLayout(false);
+            this.Top_ExpandPanel.ResumeLayout(false);
+            this.Left_Panel.ResumeLayout(false);
+            this.TopLogo_Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.TopLogo_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Mid_Panel;
+        private System.Windows.Forms.Panel Mid_ExpandPanel;
+        private System.Windows.Forms.Panel Top_Panel;
+        private System.Windows.Forms.Panel Top_ExpandPanel;
+        private System.Windows.Forms.Panel Left_Panel;
+        private System.Windows.Forms.Panel Left_ExpandPanel;
+        private System.Windows.Forms.Panel TopLogo_Panel;
+        private System.Windows.Forms.Panel MidUserControl_Panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox TopLogo_PictureBox;
+    }
+}
+
--- MovieX_MainMenu.cs
+++ MovieX_MainMenu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MovieX
 {
-    public partial class Form1 : Form
+    public partial class MovieX_MainMenu : Form
     {
-        public Form1()
+        public MovieX_MainMenu()
         {
             InitializeComponent();
         }
